3500268f6f2e3de34d6016c9e6bdb672dac5e431
Maurice <riciex@hotmail.com>
2017-05-22 15:06:45 +0200

    Added order list, order details and an order combine page

--- app/controllers/ordersList.php
+++ app/controllers/ordersList.php
@@ -12,6 +12,28 @@
 			$this->view('general/copyright', ['end_date' => date('Y') + 1]);
 		}
 		
+		public function details($id)
+		{
+			$menuModel = $this->model('MenuModel');
+			$orderModel = $this->model('OrderModel');
+			$this->view('general/menu', ['menu_items' => $menuModel->items]);
+			$this->view('general/backtotop');
+			$this->view('orders/orderdetails', ['orderrequestrows' => $orderModel->getOrderRequestDetails($id), 'orderrequest' => $orderModel->getOrderRequest($id)]);
+			$this->view('general/footer');
+			$this->view('general/copyright', ['end_date' => date('Y') + 1]);
+		}
+		
+		public function combined()
+		{
+			$menuModel = $this->model('MenuModel');
+			$orderModel = $this->model('OrderModel');
+			$this->view('general/menu', ['menu_items' => $menuModel->items]);
+			$this->view('general/backtotop');
+			$this->view('orders/combinedorders', ['orderrequests' => $orderModel->getCombinedOrderRequestDetails(), 'suppliers' => $orderModel->getSuppliers()]);
+			$this->view('general/footer');
+			$this->view('general/copyright', ['end_date' => date('Y') + 1]);
+		}
+		
 		public function test()
 		{
 			krumo($_POST);
